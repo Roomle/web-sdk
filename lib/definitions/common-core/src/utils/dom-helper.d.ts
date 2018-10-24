d0efa0e22aa35707fd50c4aa2e866f47cea9459a
roomle-build <georg.kothmeier+roomle-build@roomle.com>
2018-10-24 16:29:39 +0200

    [RELEASE] 1.0.5

--- lib/definitions/common-core/src/utils/dom-helper.d.ts
+++ lib/definitions/common-core/src/utils/dom-helper.d.ts
@@ -9,10 +9,12 @@ export default class DomHelper {
     private _element;
     private _container;
     private _canvas;
+    private _resizeListener;
     readonly element: HTMLElement;
     canvas: HTMLCanvasElement;
     setDomElement(element: HTMLElement): void;
     private _setClientDimensions;
     getClientDimensions(): THREE.Vector2;
     cleanUp(): void;
+    reset(): void;
 }
