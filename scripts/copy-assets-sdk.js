e122941dfe82d7e7863ac51f7257faa41c64b80c
roomle-build <georg.kothmeier+roomle-build@roomle.com>
2018-10-25 12:40:59 +0200

    [RELEASE] 1.0.8

--- scripts/copy-assets-sdk.js
+++ scripts/copy-assets-sdk.js
@@ -1,8 +1,73 @@
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
 const packageJson = require('../../../package.json');
 
-const roomleAssetTarget = (packageJson.roomle) ? path.resolve(__dirname, '..', '..', '..', packageJson.roomle.assetTarget) : undefined;
+// This ceremony is needed due to:
+// http://krasimirtsonev.com/blog/article/Fun-playing-with-npm-dependencies-and-postinstall-script
+
+const mkDirByPathSync = (targetDir, {isRelativeToScript = false} = {}) => {
+    const sep = path.sep;
+    const initDir = path.isAbsolute(targetDir) ? sep : '';
+    const baseDir = isRelativeToScript ? __dirname : '.';
+
+    return targetDir.split(sep).reduce((parentDir, childDir) => {
+        const curDir = path.resolve(baseDir, parentDir, childDir);
+        try {
+            fs.mkdirSync(curDir);
+        } catch (err) {
+            if (err.code === 'EEXIST') {
+                return curDir;
+            }
+
+            if (err.code === 'ENOENT') {
+                throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+            }
+
+            const caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
+            if (!caughtErr || caughtErr && curDir === path.resolve(targetDir)) {
+                throw err;
+            }
+        }
+
+        return curDir;
+    }, initDir);
+}
+
+const copyFileSync = (source, target) => {
+
+    let targetFile = target;
+
+    if (fs.existsSync(target)) {
+        if (fs.lstatSync(target).isDirectory()) {
+            targetFile = path.join(target, path.basename(source));
+        }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+};
+
+const copyFolderRecursiveSync = (source, target) => {
+    let files = [];
+
+    const targetFolder = path.join(target, path.basename(source));
+    if (!fs.existsSync(targetFolder)) {
+        mkDirByPathSync(targetFolder);
+    }
+
+    if (fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach(function (file) {
+            var curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder);
+            } else {
+                copyFileSync(curSource, targetFolder);
+            }
+        });
+    }
+}
+
+const roomleAssetTarget = (packageJson.roomle && packageJson.roomle.assetTarget) ? path.resolve(__dirname, '..', '..', '..', packageJson.roomle.assetTarget) : undefined;
 
 if (!roomleAssetTarget) {
     console.log('[Roomle]: Warning, you did not provide an asset target in your package.json');
@@ -11,4 +76,4 @@ if (!roomleAssetTarget) {
 }
 const src = path.resolve(__dirname, '..', 'lib', 'static');
 console.log('[Roomle]: copy roomle assets from: ' + src + ' to ' + roomleAssetTarget);
-fs.copySync(src, roomleAssetTarget);
+copyFolderRecursiveSync(src, roomleAssetTarget);
